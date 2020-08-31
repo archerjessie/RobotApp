8fa0c0923157c4a9c6c35e2b841bca0d70c659e0
archerjessie <66236559+archerjessie@users.noreply.github.com>
2020-08-31 22:09:38 +1000

    first commit

---
+++ src/main/java/com/assignment/jr/RobotApp/RobotAppApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.jr.RobotApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/jr/RobotApp/RobotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.jr.RobotApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
